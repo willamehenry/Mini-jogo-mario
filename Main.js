486778813892288e53606c7e7a2ca87e21e32841
willamehenry <willamehenry1996@gmail.com>
2023-09-20 12:02:55 -0300

    subindo alteração som 1.6

--- Main.js
+++ Main.js
@@ -4,8 +4,8 @@ const pipe = document.querySelector('.pipe')
 const start = document.querySelector('.start')
 const gameOver = document.querySelector('.game-over')
 
-audioStart = new Audio('/soung/audio_theme.mp3')
-audioGameOver = new Audio('/soung/audio_gameover.mp3')
+audioStart = new Audio('../soung/audio_theme.mp3')
+audioGameOver = new Audio('../soung/audio_gameover.mp3')
 
 
 const startGame = () => {
