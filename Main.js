095f1843c835e399a6d0a2ef9fe9a1ee9c6141b2
willamehenry <willamehenry1996@gmail.com>
2023-09-20 15:31:02 -0300

    Atualizacao 2.0

--- Main.js
+++ Main.js
@@ -4,13 +4,14 @@ const pipe = document.querySelector('.pipe')
 const start = document.querySelector('.start')
 const gameOver = document.querySelector('.game-over')
 
-audioStart = new Audio('../soung/audio_theme.mp3')
-audioGameOver = new Audio('../soung/audio_gameover.mp3')
+audioStart = new Audio('../soung/audio_theme.mp3');
+audioGameOver = new Audio('../soung/audio_gameover.mp3');
 
 
 const startGame = () => {
     pipe.classList.add('pipe-animation')
     start.style.display = 'none'
+   
 
     // audio
     audioStart.play()
